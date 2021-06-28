b5495a241cd9f8784b44c556f7bf8ec843b2cf81
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-06-28 15:50:50 -0500

    Some more boilerplate, also thermal unification

--- modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/recipies/replace_input.js
+++ modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/recipies/replace_input.js
@@ -1,4 +1,13 @@
 onEvent("recipes", (event) => {
     // Replace Raritanium with good-old copper
     event.replaceInput({}, "calemiutils:raritanium", "#forge:ingots/copper");
+
+    // Thermal
+    event.replaceInput({}, 'thermal:cinnabar', '#forge:gems/cinnabar');
+    event.replaceInput({}, 'thermal:sulfur', '#forge:gems/sulfur');
+    event.replaceInput({}, 'thermal:apatite', '#forge:gems/apatite');
+    event.replaceInput({}, 'thermal:niter', '#forge:gems/niter');
+    event.replaceInput({}, 'thermal:bitumen', '#forge:gems/bitumen', true);
+    event.replaceInput({}, 'thermal:coal_coke', '#forge:gems/coal_coke');
+    event.replaceInput({}, 'thermal:slag', '#forge:slag');
 });

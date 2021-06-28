c6a8cf197d0d95b1396e27f475bfffe9521bbc76
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-06-28 15:45:20 -0500

    Replace some recipe outputs

---
+++ modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/recipies/replace_output.js
@@ -0,0 +1,9 @@
+onEvent("recipes", (event) => {
+    event.replaceOutput({}, 'thermal:cinnabar', '#forge:gems/cinnabar');
+    event.replaceOutput({}, 'thermal:sulfur', '#forge:gems/sulfur');
+    event.replaceOutput({}, 'thermal:apatite', '#forge:gems/apatite');
+    event.replaceOutput({}, 'thermal:niter', '#forge:gems/niter');
+    event.replaceOutput({}, 'thermal:bitumen', '#forge:gems/bitumen');
+    event.replaceOutput({}, 'thermal:coal_coke', '#forge:gems/coal_coke');
+    event.replaceOutput({}, 'thermal:sawdust', 'emendatusenigmatica:wood_dust');
+})

db99f81d33b663eeea38db023fbe2c945cb8bacf
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-06-28 20:10:05 -0500

    Formatting

--- modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/recipes/shaped.js
+++ modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/recipes/shaped.js
@@ -2,29 +2,49 @@ onEvent("recipes", (event) => {
   /*
    *  F O O D
    */
-  // Waffles
-  event.shaped("kubejs:waffle", [" S ", "WWW"], {
+  /* Waffles */
+  // prettier-ignore
+  event.shaped("kubejs:waffle", [
+    " S ",
+    "WWW"
+  ], {
     S: "kubejs:syrup_bottle",
     W: "minecraft:wheat",
   });
-  event.shaped("2x kubejs:waffle", [" S ", "WWW"], {
+  // prettier-ignore
+  event.shaped("2x kubejs:waffle", [
+    " S ",
+    "WWW"
+  ], {
     S: "minecraft:honey_bottle",
     W: "minecraft:wheat",
   });
-  // Pizza
-  event.shaped("kubejs:pizza", ["CTC", "WWW"], {
+  /* Pizza */
+  // prettier-ignore
+  event.shaped("kubejs:pizza", [
+    "CTC",
+    "WWW"
+  ], {
     C: "kubejs:cheese",
     T: "#forge:crops/tomato",
     W: "minecraft:wheat",
   });
-  // Calzone
-  event.shaped("2x kubejs:calzone", ["WWW", "CTC", "WWW"], {
+  /* Calzone */
+  // prettier-ignore
+  event.shaped("2x kubejs:calzone", [
+    "WWW",
+    "CTC",
+    "WWW"
+  ], {
     C: "kubejs:cheese",
     T: "#forge:crops/tomato",
     W: "minecraft:wheat",
   });
-  // Not-bread
-  event.shaped("kubejs:bread", ["WDW"], {
+  /* Not-bread */
+  // prettier-ignore
+  event.shaped("kubejs:bread", [
+    "WDW"
+  ], {
     W: "minecraft:wheat",
     D: "#forge:dusts/wood",
   });

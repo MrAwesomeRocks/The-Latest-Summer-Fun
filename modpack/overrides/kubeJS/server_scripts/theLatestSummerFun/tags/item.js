47fa4d9c9f1cb626eb3d4857995cc8651f32a147
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-06-29 11:12:01 -0500

    Tag jelly and jam

--- modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/tags/item.js
+++ modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/tags/item.js
@@ -1,1 +1,15 @@
-onEvent("item.tags", (event) => {});
+/*
+ * Add item tags.
+ *
+ * Templates:
+ *  - `event.add(tag, item)`
+ *  - `event.remove(tag, item)`
+ *  - `event.removeAll(tag)`
+ *
+ * See https://mods.latvian.dev/books/kubejs/page/tageventjs for more info.
+ */
+onEvent("item.tags", (event) => {
+  // Jelly/Jam tags
+  event.add("forge:jelly_or_jam", "kubejs:jelly");
+  event.add("forge:jelly_or_jam", "upgrade_aquatic:mulberry_jam_bottle");
+});

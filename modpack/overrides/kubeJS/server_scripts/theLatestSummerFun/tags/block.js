ca221d9f9d606ed05c690f2413662211c742ede0
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-06-30 14:12:57 -0500

    Comments

--- modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/tags/block.js
+++ modpack/overrides/kubeJS/server_scripts/theLatestSummerFun/tags/block.js
@@ -1,1 +1,11 @@
+/*
+ * Add block tags.
+ *
+ * Templates:
+ *  - `event.add(tag, item)`
+ *  - `event.remove(tag, item)`
+ *  - `event.removeAll(tag)`
+ *
+ * See https://mods.latvian.dev/books/kubejs/page/tageventjs for more info.
+ */
 onEvent("block.tags", (event) => {});

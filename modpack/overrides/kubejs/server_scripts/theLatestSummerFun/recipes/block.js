879bd6eeb775fb3149067c083238024a98ef8047
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-07-05 14:57:30 -0500

    Add gem -> gear recipes
    Fixes #4

--- modpack/overrides/kubejs/server_scripts/theLatestSummerFun/recipes/block.js
+++ modpack/overrides/kubejs/server_scripts/theLatestSummerFun/recipes/block.js
@@ -9,9 +9,31 @@
  ** See L36 of  https://mods.latvian.dev/books/kubejs/page/recipeeventjs for more details.
  */
 onEvent("recipes", (event) => {
-  /*
-   *  F O O D
-   */
+  //! Gem -> gear compressing
+  const gems = ["diamond", "emerald", "lapis", "quartz"];
+
+  for (let gem of gems) {
+    event.recipes.thermal.press(`#forge:gears/${gem}`, [
+      `4x #forge:gems/${gem}`,
+      "thermal:press_gear_die",
+    ]);
+    event.recipes.immersiveengineering.metal_press(
+      `#forge:gears/${gem}`,
+      `4x #forge:gems/${gem}`,
+      "immersiveengineering:mold_gear"
+    );
+  }
+
+  //! F O O D
+  // Syrup
+  event.recipes.thermal
+    .bottler("kubejs:syrup_bottle", ["minecraft:glass_bottle", Fluid.of("thermal:syrup", 250)])
+    .energy(2000);
+  // Peanut butter
+  event.recipes.thermal
+    .pulverizer("2x kubejs:peanut_butter", "#forge:crops/peanut")
+    .experience(0)
+    .energy(2000);
   // Popcorn
   event.smelting("kubejs:popcorn", "#forge:crops/corn").xp(0.5).cookingTime(200);
   // Onion Rings

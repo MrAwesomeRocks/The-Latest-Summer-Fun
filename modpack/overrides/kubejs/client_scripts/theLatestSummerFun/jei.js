e56eb17884d24155bcf1c7db58b58b629ee5115b
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-07-27 09:21:11 -0500

    Fix slag hiding

--- modpack/overrides/kubejs/client_scripts/theLatestSummerFun/jei.js
+++ modpack/overrides/kubejs/client_scripts/theLatestSummerFun/jei.js
@@ -3,6 +3,6 @@
  *
  * See https://mods.latvian.dev/books/kubejs/page/jei-integration for more info.
  */
-ondeviceorientation("jei.hide.items", (event) => {
+onEvent("jei.hide.items", (event) => {
   event.hide("immersiveengineering:slag");
 });
